371f97a0c35b6210bb24020289a2ddb770691d1f
Pedro <pedrositta15@gmail.com>
2026-08-11 11:47:23 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 /* ==========================================================================
-   SnapFlow - Progressive Web App Service Worker (v37 Live Network-First)
+   SnapFlow - Progressive Web App Service Worker (v41 Live Network-First)
    ========================================================================== */
 
-const CACHE_NAME = 'snapflow-pwa-v37';
+const CACHE_NAME = 'snapflow-pwa-v41';
 
 // Install Event - Força atualização imediata
 self.addEventListener('install', (event) => {
